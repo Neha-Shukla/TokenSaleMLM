84473a3846b9ec580db9da1c5f8381dd1931758b
neha-decrypt <neha.decrypt@gmail.com>
2023-03-11 22:39:17 +0530

    fixing

--- src/app/dashboard/Dashboard.js
+++ src/app/dashboard/Dashboard.js
@@ -222,7 +222,7 @@ function Dashboard() {
               <div onClick={() => {
                 handleCopyToClipboard(`https://lucreway.com/refAdd/${account}`)
               }}>
-                <input type="text" className='refferalLink' disabled={true} value={account ? `https://lucreway.com/${account}` : ""}></input>
+                <input type="text" className='refferalLink' disabled={true} value={account && income?.data?.tokensReceived ? `https://lucreway.com/${account}` : ""}></input>
               </div>
             </div>
           </div>
