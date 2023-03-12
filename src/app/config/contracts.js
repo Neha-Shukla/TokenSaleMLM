d402573d74c27b4f21ec2521bab412b5cd8debb3
neha-decrypt <neha.decrypt@gmail.com>
2023-03-12 12:10:11 +0530

    fixing

--- src/app/config/contracts.js
+++ src/app/config/contracts.js
@@ -1,3 +1,3 @@
-export const tokenSale = "0xC1c3DCa58574447bAA559d5A399A03233aF8CFfC";
+export const tokenSale = "0xF3AAdF10284Eae1af2E4250b75E0d9f427713fb9";
 export const TokenBEP20 = "0xB8c1969f5ED3b196dF6aeb861122e88C3B032f8B";
 export const PriceFeed = "0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526";
